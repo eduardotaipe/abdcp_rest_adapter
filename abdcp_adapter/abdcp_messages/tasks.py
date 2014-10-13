be9c5b1a66ee6e9d0bea3193a04f29df3dc05c22
John Montero <jmontero@rcp.pe>
2014-10-13 12:07:46 -0500

    Adding send message task

--- abdcp_adapter/abdcp_messages/tasks.py
+++ abdcp_adapter/abdcp_messages/tasks.py
@@ -1,10 +1,14 @@
 import logging
+import requests
+import json
+import base64
 
 from celery import shared_task
 from django.core.mail import EmailMessage
 from django.template import Context
 from django.template.loader import get_template
 
+from abdcp_messages.models import ABDCPMessage
 from abdcp_adapter import settings
 
 @shared_task
@@ -15,3 +19,59 @@ def send_email(info):
     msg = EmailMessage(info["subject"], body, settings.SENDER_EMAIL,[info["to"]])
     msg.content_subtype = "html"
     msg.send()
+
+
+@shared_task
+def send_message(message_id):
+    retry = False
+    try:
+        logging.info("=== Begin ABDCP message send ===")
+        message = ABDCPMessage.objects.get(message_id=message_id)
+        result = ""
+
+	payload = {
+            "userID" : settings.ABDCP_CLIENT_REST_ADAPTER_USR,
+            "password" : base64.b64encode(settings.ABDCP_CLIENT_REST_ADAPTER_PWD),
+            "xmlMsg" : message.response_document,
+            "attachedDoc" : None 
+        }
+
+	r = requests.post(settings.ABDCP_CLIENT_REST_ADAPTER_ENDPOINT, 
+                          data=json.dumps(payload), verify=settings.ABDCP_CLIENT_REST_ADAPTER_VERIFY_SSL,
+                          headers={
+                             'Accept':'application/json',
+                             'Content-Type':'application/json; charset=utf-8' 
+                          }
+        )
+
+        if r.status_code == requests.codes.ok:
+            response = json.loads(r.text)
+            
+            if "response" in response and response['response'][0:3]=='ERR':
+                result = response['response']
+            else:
+                result = "Message %s (%s) has been sended" % \
+                    (message.message_id,message.message_type)
+            
+                message.mark_delivered()
+        else:
+            result = "Message %s (%s) has NOT been sended, only stored\nError: %s"%\
+                (message.message_id,message.message_type, r.raise_for_status())
+    except ABDCPMessage.DoesNotExist, e:
+        result = "message id %s was not found" % message_id
+    except Exception, e:
+        result = "Error: processing message %s %s" % (message.message_id,e)
+        retry = True
+
+    logging.info(result)
+
+    if retry:
+        send_message.retry(
+            [message_id] ,
+            max_retries = settings.CELERY_MAX_RETRIES,
+            countdown=settings.CELERY_COUNTDOWN
+        )
+
+    logging.info("=== End ABDCP message sended ===")
+
+    return result 
